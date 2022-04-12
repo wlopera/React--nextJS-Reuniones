115e66ef4310e7ff6bbefc4ad2a93cf3e3160ed9
lopera.william@gmail.com <William>
2022-04-12 17:53:59 -0500

    uso de funciones del lado del servidor y api de mongoDB

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
       title="Una Primera reunión"
@@ -10,6 +10,43 @@ const MeetupDetails = () => {
       description="Descripción de mi porimera reunión..."
     />
   );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+
+  console.log("Consultar data del id:", meetupId);
+  // Consultar API o BD con fetch simple
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "Una Primera reunión",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "Vista Hermosa. 34 5674. Ciudad de Panamá. Panamá",
+        description: "Descripción de mi porimera reunión...",
+      },
+    },
+  };
 };
 
 export default MeetupDetails;
